2d8d1ccc40a19f459e5e1fee9dbfab6ca0c5cf41
Bart Massey <bart.massey@gmail.com>
2022-01-07 00:11:57 -0800

    changed prng to fastrand and reran benches

--- charvec.rs
+++ charvec.rs
@@ -1,5 +1,4 @@
-use rand::prelude::*;
-use rand_pcg::Pcg64Mcg;
+use fastrand::*;
 
 #[derive(Debug, Clone, Copy)]
 enum Charset {
@@ -8,40 +7,40 @@ enum Charset {
 }
 
 impl Charset {
-    fn gen_char(self, rng: &mut Pcg64Mcg) -> char {
+    fn gen_char(self, rng: &mut FastRand) -> char {
         match self {
-            Self::Unicode => rng.gen(),
-            Self::Ascii => rng.gen_range(0u8 as char..=0xffu8 as char),
+            Self::Unicode => rng.random(),
+            Self::Ascii => rng.rand_range(0u8 as char, char::try_from(0x100u32).unwrap()),
         }
     }
 }
 
-fn bench_string(rng: &mut Pcg64Mcg, charset: Charset, n: usize, m: usize) -> u32 {
+fn bench_string(rng: &mut FastRand, charset: Charset, n: usize, m: usize) -> u32 {
     let index: String = (0..m).map(|_| charset.gen_char(rng)).collect();
     let mut result = 0;
     for _ in 0..n {
-        let i = rng.gen_range(0..m);
+        let i = rng.rand_range(0, m);
         let c = index.chars().nth(i).unwrap();
         result ^= c as u32;
     }
     result
 }
 
-fn bench_charvec(rng: &mut Pcg64Mcg, charset: Charset, n: usize, m: usize) -> u32 {
+fn bench_charvec(rng: &mut FastRand, charset: Charset, n: usize, m: usize) -> u32 {
     let index: Vec<char> = (0..m).map(|_| charset.gen_char(rng)).collect();
     let mut result = 0;
     for _ in 0..n {
-        let i = rng.gen_range(0..m);
+        let i = rng.rand_range(0, m);
         let c = index[i];
         result ^= c as u32;
     }
     result
 }
 
-fn bench_baseline(rng: &mut Pcg64Mcg, _charset: Charset, n: usize, m: usize) -> u32 {
+fn bench_baseline(rng: &mut FastRand, _charset: Charset, n: usize, m: usize) -> u32 {
     let mut result = 0;
     for _ in 0..n {
-        let i: usize = rng.gen_range(0..m);
+        let i: usize = rng.rand_range(0, m);
         result ^= i as u32;
     }
     result
@@ -63,7 +62,6 @@ fn main() {
     let n = args[3].parse().unwrap();
     let m = args[4].parse().unwrap();
 
-    // XXX Initialize RNG with "default" PCG state.
-    let mut rng = Pcg64Mcg::new(0xcafef00dd15ea5e5);
+    let mut rng = FastRand::default();
     println!("{}", algorithm(&mut rng, charset, n, m));
 }
